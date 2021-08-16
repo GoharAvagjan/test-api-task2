fb7b7fc27365027fdd6df3347c4c6b320cbcf054
goharavagyan <laura.avagyan@gmail.com>
2021-08-16 16:09:11 +0400

    changed port

--- sever.js
+++ sever.js
@@ -4,21 +4,21 @@ const router = require('./router');
 const mongoose = require('mongoose');
 const app = express();
 
+const PORT = process.env.PORT || 2021;
 
-app.set('port', 2021);
+app.set('port', PORT);
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 router(app);
 
 const server = http.createServer(app);
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb://localhost/task-2', {
+mongoose.connect(uri || 'mongodb://localhost/task-2', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true
 }).then(()=>{
-    server.listen(2021);
+    server.listen(PORT);
 })
-
-
